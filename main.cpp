d12d6308cafb344a32c35df7015a48bedb9f3ede
Vin <maharjanebik061@gmail.com>
2023-02-02 16:05:30 +0545

    final modification

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 #include<fstream>
+#include<iomanip>
+#include<tuple>
+#include<string>
+#include<stdlib.h>
+#include<unistd.h>
 
 using namespace std;
 
@@ -22,7 +27,7 @@ protected:
     }
 public:
     virtual string getRandomQuestion() = 0;
-    virtual void displayOptions() = 0;
+    virtual tuple<string, string, string, string> getOptions() = 0;
     virtual int getQuestionNumber() = 0;
     virtual int getAnswer() = 0;
 };
@@ -38,64 +43,12 @@ protected:
         return num;
     }
 public:
-    // Constructor
-    QuestionLevel1(){
-        this->filename = "level1.txt";
-        this->answerFile = "answer1.txt";
-    }
+    QuestionLevel1();                                       // Constructor
 
-    // Returns a random question
-    string getRandomQuestion(){
-        this->questionNumber = getRandomNum(1, 2);
-        ifstream file(this->filename);
-        while(getline (file, this->q)){
-            int num = grabQuestionNumber();
-            
-            if (this->questionNumber == num){
-                file.close();
-                return this->q;
-            }
-        }
-        return "Error";
-    }
-
-    // Displays the options
-    void displayOptions(){
-        ifstream file(this->filename);
-        while(getline (file, this->q)){
-            int num = grabQuestionNumber();
-
-            if (this-> questionNumber == num){
-                for(int i = 0; i<4; i++)
-                    getline(file, this->option[i]);
-                file.close();
-            }
-        }
-        cout << "1 " << option[0] << "\t\t" << "2 " << option[1] << endl; 
-        cout << "3 " << option[2] << "\t\t" << "4 " << option[3] << endl; 
-    }
-
-    // Get the question number of the chosen question
-    int getQuestionNumber(){
-        return this->questionNumber;
-    }
-
-    // Get the answer of the chosed question
-    int getAnswer(){
-        ifstream file(this->answerFile);
-        while (getline (file, this->a)){
-            int num = 0;
-            num = int(this->a[0]) - 48;
-            if (this->a[1] != ' ')
-                num = num * 10 + (int(this->a[1]) - 48);
-
-            if(num == this->questionNumber)    
-                return int(this->a[this->a.length() - 1]) - 48;     // The last character as integer which is our option
-        }
-
-        // Error
-        return -1;
-    }
+    string getRandomQuestion();                             // Returns a random question
+    tuple<string, string, string, string> getOptions();     // return 4 options as 4-tuple
+    int getQuestionNumber();                                // Get the question number of the chosen question
+    int getAnswer();                                        // Get the answ er of the chosed question
 };
 
 class QuestionLevel2: public QuestionLevel1{
@@ -122,14 +75,169 @@ class QuestionLevel4: public QuestionLevel1{
     }
 };
 
+
+
+// QuestionLevel1 Function Definitions
+// =====================================================
+string QuestionLevel1::getRandomQuestion(){
+        this->questionNumber = getRandomNum(1, 2);
+        ifstream file(this->filename);
+        while(getline (file, this->q)){
+            int num = grabQuestionNumber();
+            
+            if (this->questionNumber == num){
+                file.close();
+                return this->q;
+            }
+        }
+        return "Error";
+    }
+
+tuple<string, string, string, string> QuestionLevel1::getOptions(){
+    ifstream file(this->filename);
+    while(getline (file, this->q)){
+        int num = grabQuestionNumber();
+        if (this-> questionNumber == num){
+            for(int i = 0; i<4; i++)
+                getline(file, this->option[i]);
+            file.close();
+        }
+    }
+    return make_tuple(this->option[0], this->option[1], this->option[2], this->option[3]);
+}
+
+int QuestionLevel1::getQuestionNumber(){
+    return this->questionNumber;
+}
+
+int QuestionLevel1::getAnswer(){
+    ifstream file(this->answerFile);
+    while (getline (file, this->a)){
+        int num = 0;
+        num = int(this->a[0]) - 48;
+
+        if (this->a[1] != ' ')
+            num = num * 10 + (int(this->a[1]) - 48);
+        
+        if(num == this->questionNumber)    
+            return int(this->a[this->a.length() - 1]) - 48;     // The last character as integer which is our option
+    }
+    // Error
+    return -1;
+}
+
+QuestionLevel1::QuestionLevel1(){
+    this->filename = "level1.txt";
+    this->answerFile = "answer1.txt";
+}
+
+// ==================================================
+// End of QuestionLevel1 Function Definition
+
+
+
+void symbolPrinter(char symbol, int no){
+    int i;
+    for (i=0; i<no; ++i)
+        cout << symbol;
+    cout << endl;
+}
+
+
+
+class Structure{
+private:
+    int p;
+    int checkpoints[3];
+public:
+    // Constructor
+    Structure(){
+        this->p = 15;
+        this->checkpoints[0] = 4;
+        this->checkpoints[1] = 8;
+        this->checkpoints[2] = 15;
+    }
+
+    // Display the structure
+    void display(){
+        symbolPrinter('|', 70);
+        symbolPrinter('.', 70);
+        cout << "Lifeline" << setw(42) << "X2(Double 5)\t>>(Next 6)" << endl;
+        symbolPrinter('.', 70);
+
+        string money[] = {"5K", "10K", "20K", "40K", "80K", "1.6Lakh", "3.2Lakh", "6.4Lakh", "12.5Lakh", "25Lakh", "50Lakh", "1 Crore", "3 Crore", "5 Crore", "7 Crore"};
+        int i;
+        for(i = 15; i>0; i--){
+            int c = i - 1;
+            int w = 30;
+            string position = to_string(i);
+
+            if (i < 10)
+                w = 31;
+
+            if (i == 15 || i == 8 || i == 4){
+                w = 29;
+                position = "(" + position + ")";
+            }
+
+
+            cout << position;
+            if (i == p){
+                w-=2;
+                cout << setw(w-10) << ">>" << setw(12) << money[c] << endl;
+            }
+            else cout << setw(w) << money[c] << endl;
+        }
+        symbolPrinter('.', 70);
+        this->p++;
+    }
+
+    // Return the current position
+    int getCurrentPosition(){
+        return this->p - 1;
+    }
+
+    // Return the last checkpoint
+    int getCheckpoint(){
+        int cp = 0;
+        for (int i=0; i<3; ++i){
+            if(this->p >= this->checkpoints[i]){
+                cp = this->checkpoints[i];
+                
+            }
+        }
+        return cp;
+    }
+};
+
+
+
 int main(){
-    QuestionLevel2 q1;
+    QuestionLevel1 q1;
+    Structure s;
 
-    cout<<q1.getRandomQuestion()<< endl;
-    q1.displayOptions();
-    cout << q1.getQuestionNumber() << endl;
-    cout << q1.getAnswer() << endl;
+    // Display the structure ( Use a loop, the position of '>>' will automatically change in each iteration)
+    s.display();
 
+    // Get a random question
+    string question = q1.getRandomQuestion();
     
+    // Display that question
+    cout << question << endl;
+
+    // Get the options
+    tuple<string, string, string, string> options = q1.getOptions();
+    
+    // Display the options
+    cout << get<0>(options) << "\t\t\t\t" << get<1>(options) << endl;
+    cout << get<2>(options) << "\t\t\t\t" << get<3>(options) << endl;
+    
+
+    // Get Current Position in the structure (1 to 15)
+    cout << s.getCurrentPosition() << endl;
+
+    // Get Last Checkpoint in the structure (4 or 8 or 15)
+    cout << s.getCheckpoint() << endl;
+
     return 0;
 }
